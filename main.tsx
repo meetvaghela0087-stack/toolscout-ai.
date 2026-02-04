0360e82712dced7380b230ea5a92b41be5498557
meetvaghela0087-stack <meetvaghela0087@gmail.com>
2026-02-04 12:47:43 +0530

    Fix App import statement syntax
    Removed curly braces from App import statement.

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { App } from "./App";
+import App from "./App"; // Yahan se maine { } hata diye hain
 
 const rootElement = document.getElementById("root");
 
